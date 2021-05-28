e9b0dcb431d587524c9f7faa3c9635329072230c
Matts Norrbohm <matts.norrbohm@gmail.com>
2021-05-28 14:34:42 +0200

    Added Updater function also to the Node-API interface

--- c-template-n-api/exos_template.js
+++ c-template-n-api/exos_template.js
@@ -6,6 +6,7 @@ const ar = require('../template/ar');
 const template_ar = require('../c-template/exos_template_ar');
 const template_linux = require('./exos_template_linux_n_api');
 const path = require('path');
+var XMLparse = require('../node_modules/xml-parser');
 
 function generateTemplate(fileName, structName, outPath) {
 
@@ -94,21 +95,108 @@ function generateTemplate(fileName, structName, outPath) {
     fs.writeFileSync(`${outPath}/${structName}/Linux/package-lock.json`, out);
 }
 
-if (require.main === module) {
+function updateTemplate(fileName) {
+    try {
+        var xml = fs.readFileSync(fileName).toString();
+    } catch {
+        throw ('Error: could not read .exospkg file')
+    }
 
-    if (process.argv.length > 3) {
-        outPath = process.argv[4];
-        if (outPath == "" || outPath == undefined) {
-            outPath = ".";
+    try {
+        var obj = XMLparse(xml);
+    } catch {
+        throw ('Error: could not parse .exospkg file');
+    }
+
+    let typFile = "";
+    let structName = "";
+    let libName = "";
+    let outPath = "";
+
+    for (let child of obj.root.children) {
+        if (child.name == "Build") {
+            for (let build of child.children) {
+                if (build.name == "GenerateHeader") {
+                    if (build.attributes["FileName"] != null && build.attributes["TypeName"] != null) {
+                        typFile = path.join(path.dirname(fileName), build.attributes["FileName"].replace(/\\/g, '/'));
+                        structName = build.attributes["TypeName"];
+                        libName = structName.substring(0, 10);
+                        outPath = path.dirname(path.dirname(fileName));
+                        break;
+                    }
+                    break;
+                }
+            }
+            break;
         }
-        let fileName = process.argv[2];
-        let structName = process.argv[3];
+    }
 
-        try {
-            generateTemplate(fileName, structName, outPath);
-            process.stdout.write(`exos_template ${structName} generated at ${outPath}`);
-        } catch (error) {
-            process.stderr.write(error);
+    //check that we have all we need
+    if (libName == "") throw (`GenerateHeader section not found in .exospkg file!`);
+
+    if (!fs.existsSync(typFile)) throw (`Typ source file ${typFile} not found`);
+
+    //mainly checking that folders exist and points to something valid
+    if (!fs.existsSync(`${path.dirname(fileName)}/${libName}/${structName.toLowerCase()}.c`)) throw (`${path.dirname(fileName)}/${libName}/${structName.toLowerCase()}.c does not exist!`);
+    if (!fs.existsSync(`${path.dirname(fileName)}/Linux/lib${structName.toLowerCase()}.c`)) throw (`${path.dirname(fileName)}/Linux/lib${structName.toLowerCase()}.c does not exist!`);
+
+    //ok were good to go, regenerate
+    let out = "";
+
+    //headers
+    out = header.generateHeader(typFile, structName, [`${libName}.h`]);
+    //AS header
+    fs.writeFileSync(path.join(outPath, structName, libName, `exos_${structName.toLowerCase()}.h`), out);
+    //Linux header
+    fs.writeFileSync(path.join(outPath, structName, 'Linux', `exos_${structName.toLowerCase()}.h`), out);
+
+    //AR library
+    out = template_ar.generateTemplate(typFile, structName);
+    fs.writeFileSync(`${outPath}/${structName}/${libName}/${structName.toLowerCase()}.c`, out);
+
+    out = template_ar.generateFun(typFile, structName);
+    fs.writeFileSync(`${outPath}/${structName}/${libName}/${libName}.fun`, out);
+
+    out = template_ar.generateCLibrary(path.basename(fileName), structName);
+    fs.writeFileSync(`${outPath}/${structName}/${libName}/ANSIC.lby`, out);
+
+    fs.writeFileSync(`${outPath}/${structName}/${libName}/dynamic_heap.cpp`, "unsigned long bur_heap_size = 100000;\n");
+
+    //Linux files
+    out = template_linux.generateLibTemplate(typFile, structName);
+    fs.writeFileSync(`${outPath}/${structName}/Linux/lib${structName.toLowerCase()}.c`, out);
+
+    return structName;
+}
+
+//used during dev. for simple testing in VS Code..
+if (require.main === module) {
+    if (process.argv.length > 3) {
+        if (process.argv[2] != "-u") {
+            outPath = process.argv[4];
+
+            if (outPath == "" || outPath == undefined) {
+                outPath = ".";
+            }
+
+            let fileName = process.argv[2];
+            let structName = process.argv[3];
+
+            try {
+                generateTemplate(fileName, structName, outPath);
+                process.stdout.write(`exos_template ${structName} generated at ${outPath}`);
+            } catch (error) {
+                process.stderr.write(error);
+            }
+        } else {
+            let fileName = process.argv[3];
+
+            try {
+                updateTemplate(fileName);
+                process.stdout.write(`exos_template ${structName} generated at ${outPath}`);
+            } catch (error) {
+                process.stderr.write(error);
+            }
         }
     }
     else {
@@ -117,5 +205,6 @@ if (require.main === module) {
 }
 
 module.exports = {
-    generateTemplate
+    generateTemplate,
+    updateTemplate
 }
